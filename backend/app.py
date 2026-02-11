821d312002dd5a8e122780d2bf6d658f6abd9205
sriharshagv-droid <sriharshagv@gmail.com>
2026-02-11 14:06:19 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from backend, application is running in EKS"
+    return "Hello from backend running in EKS"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
 
 
